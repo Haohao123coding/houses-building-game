da87b9cbf6a5e6d95ce14206bf5c006239e63bf1
Haohao123coding <18217288892@163.com>
2025-05-17 15:32:19 +0800

    Update gameMain.js

--- gameMain.js
+++ gameMain.js
@@ -1,3 +1,10 @@
+let playerStats = {
+    wood: 0,
+    brick: 0,
+    sand: 0,
+    money: 0
+}
+
 function showDifficulty(){
     document.querySelector(".difficulty-board").style.display = "block";
     document.querySelector(".start-board").style.display = "none";
@@ -9,5 +16,19 @@ function backToStart(){
 }
 
 function startGame(difficulty){
-    // todo
+    let startResource;
+    switch(difficulty){
+        case 1:
+            startResource = 10;
+        case 2:
+            startResource = 5;
+        case 3:
+            startResource = 3;
+        case 4:
+            startResource = 0;
+    }
+    wood = startResource;
+    brick = startResource;
+    sand = startResource;
+    money = startResource;
 }
