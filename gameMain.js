c97ffea73340851add511dd7474c1db589fc2f21
Haohao123coding <18217288892@163.com>
2025-05-17 11:04:39 +0800

    updates to buttons

--- gameMain.js
+++ gameMain.js
@@ -1,3 +1,9 @@
-function showDifficuty(){
-    document.getElementById("difficulty-board").style.display = "block";
+function showDifficulty(){
+    document.querySelector(".difficulty-board").style.display = "block";
+    document.querySelector(".start-board").style.display = "none";
+}
+
+function backToStart(){
+    document.querySelector(".start-board").style.display = "block";
+    document.querySelector(".difficulty-board").style.display = "none";
 }
