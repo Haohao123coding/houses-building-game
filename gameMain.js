27fd9779caf341acc812ad60c2072e5af316fed2
Haohao123coding <18217288892@163.com>
2025-05-17 18:24:03 +0800

    Update allocate part 2

--- gameMain.js
+++ gameMain.js
@@ -1,9 +1,9 @@
-let playerStats = {
-    wood: 0,
-    brick: 0,
-    sand: 0,
-    money: 0
-}
+let wood = 0;
+let brick = 0;
+let stone = 0;
+let money = 0;
+let startResource = 0;
+let additionalResource = 0;
 
 function showDifficulty(){
     document.querySelector(".difficulty-board").style.display = "block";
@@ -15,25 +15,57 @@ function backToStart(){
     document.querySelector(".difficulty-board").style.display = "none";
 }
 
-function startGame(difficulty){
-    let startResource;
+function readyGame(difficulty){
     switch(difficulty){
         case 1:
             startResource = 10;
+            additionalResource = 15;
+            break;
         case 2:
             startResource = 5;
+            additionalResource = 10;
+            break;
         case 3:
             startResource = 3;
+            additionalResource = 5;
+            break;
         case 4:
             startResource = 0;
+            additionalResource = 0;
+            break;
     }
     wood = startResource;
     brick = startResource;
-    sand = startResource;
+    stone = startResource;
     money = startResource;
 
     document.querySelector(".start-board").style.display = "none";
     document.querySelector(".difficulty-board").style.display = "none";
 
     document.querySelector(".allocate-board").style.display = "block";
+    document.getElementById("start-resource").textContent = startResource;
+    document.getElementById("additional-resource").textContent = additionalResource;
+}
+
+function endAllocate(){
+    const values = {
+        wood: document.getElementById("wood").value,
+        brick: document.getElementById("brick").value,
+        stone: document.getElementById("stone").value
+    }
+
+    for (const [key, value] of Object.entries(values)){
+        if(!value || value.trim() === ""){
+            alert(`值不能为空！`);
+            return;
+        }
+    }
+
+    additionalWood = parseInt(document.getElementById("wood").value)
+    additionalBrick = parseInt(document.getElementById("brick").value)
+    additionalStone = parseInt(document.getElementById("stone").value)
+
+    wood += startResource;
+    brick += startResource;
+    stone += startResource;
 }
