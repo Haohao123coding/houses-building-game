70d5360835e965dd22186cec44efac4940179529
Haohao123coding <18217288892@163.com>
2025-05-25 16:29:43 +0800

    Create housePool.js

---
+++ housePool.js
@@ -0,0 +1,22 @@
+const housePool = {
+    1: {
+        name: "住宅",
+        woodDemand: 1, brickDemand: 1, stoneDemand: 1, timeDemand: 1,
+        scoreReward: 3
+    },
+    2: {
+        name: "医院",
+        woodDemand: 1, brickDemand: 2, stoneDemand: 2, timeDemand: 2,
+        scoreReward: 5
+    },
+    3: {
+        name: "工厂",
+        woodDemand: 1, brickDemand: 3, stoneDemand: 1, timeDemand: 1,
+        scoreReward: 5
+    },
+    2: {
+        name: "商业街",
+        woodDemand: 2, brickDemand: 2, stoneDemand: 3, timeDemand: 3,
+        scoreReward: 7
+    },
+}
